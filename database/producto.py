f77a6cb2963079ac524f6b4f25462112e87c21c1
Fernando <151165920+fmoorenoo@users.noreply.github.com>
2024-06-13 22:27:12 +0100

    Eliminado main de pruebas y producto.py

--- database/producto.py
+++
@@ -1,9 +0,0 @@
-class Producto:
-    def __init__(self, producto:str) -> None:
-        self.producto = producto
-
-    def __str__(self) -> str:
-        return self.producto
-    
-    def leer(self) -> str:
-        return self.producto
